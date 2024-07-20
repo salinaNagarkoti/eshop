d3613c16c7362a9ba3c9a16a18d552be8ffde164
salinaNagarkoti <salinanagarkoti55@gmail.com>
2024-07-20 09:25:35 +0545

    eShop

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Nav from './component/Nav'
+import Banner from './component/Banner'
+import Footer from './component/Footer'
+import Section from './component/Section'
+
+function App() {
+    return (
+        <>
+            <Nav />
+            <Banner />
+            <Section />
+            <Footer />
+          
+        </>
+    )
+}
+
+export default App
